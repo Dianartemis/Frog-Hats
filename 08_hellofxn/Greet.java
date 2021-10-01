b259aeda244cd561cd96830bc3590eb73f14af4d
Diana Akhmedova <dakhmedova30@stuy.edu>
2021-09-30 21:48:48 -0400

    init setup

---
+++ 08_hellofxn/Greet.java
@@ -0,0 +1,11 @@
+public class Greet{
+  public static void Greet (String name) {
+    System.out.println("Why, hello there, " + name + ". How do you do?");
+  {
+
+  public static void main (String[]args) {
+    Greet("Ajax");
+    Greet("the mightiest duck of all");
+    Greet("Dr. Ray");
+  }
+}
