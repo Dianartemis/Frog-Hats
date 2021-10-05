0c0a6e72b567e2b67bc881be9c5694a6882cf507
Diana Akhmedova <90854530+Dianartemis@users.noreply.github.com>
2021-10-05 02:54:23 -0400

    Update Greet.java

--- 09_bigsib/Greet.java
+++ 09_bigsib/Greet.java
@@ -1,3 +1,15 @@
+/*
+Diana Akhmedova
+APCS
+HW 09 - What are BigSibs good for?
+2021-10-03
+
+DISCOVERIES:
+- Public classes can be called from different Java files.
+UNRESOLVED QUESTIONS:
+- N/A
+*/
+
 public class Greet {
   public static void main (String[]args) {
     BigSib.greet("Red Head");
