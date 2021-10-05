c7c8ec4fe626b81c26f9a9deb602e52aed88753f
Diana Akhmedova <dakhmedova30@stuy.edu>
2021-10-05 03:36:24 -0400

    init setup

---
+++ 10_bigsib/BigSib.java
@@ -0,0 +1,8 @@
+public class BigSib {
+  public static String greet (String name) {
+    System.out.println("Why, hello there, " + name + ". How do you do?");
+    return " ";
+  }
+
+}
+
---
+++ 10_bigsib/Greet.java
@@ -0,0 +1,7 @@
+public class Greet {
+  public static void main (String[]args) {
+    System.out.println(BigSib.greet("Kevin"));
+    System.out.println(BigSib.greet("Oscar"));
+    System.out.println(BigSib.greet("Diana"));
+  }
+}
