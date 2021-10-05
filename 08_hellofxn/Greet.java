a2bba5fba3159af6099ccedacadcfa25750aa84d
Diana Akhmedova <90854530+Dianartemis@users.noreply.github.com>
2021-10-05 02:58:26 -0400

    Update Greet.java

--- 08_hellofxn/Greet.java
+++ 08_hellofxn/Greet.java
@@ -1,3 +1,15 @@
+/*
+Diana Akhmedova
+APCS
+HW 08 - Refactor Freshie Zero
+2021-09-30
+
+DISCOVERIES:
+- Variables are useful for efficiently outputting something multiple times.
+UNRESOLVED QUESTIONS:
+- N/A
+*/
+
 public class Greet{
   public static void Greet (String name) {
     System.out.println("Why, hello there, " + name + ". How do you do?");
