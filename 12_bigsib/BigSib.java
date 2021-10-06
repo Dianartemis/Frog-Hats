cb318a73fa1efc5e9b9ba9408f8c607da81eaf1d
Diana Akhmedova <dakhmedova30@stuy.edu>
2021-10-05 22:39:15 -0400

    init setup

---
+++ 12_bigsib/BigSib.java
@@ -0,0 +1,10 @@
+public class BigSib {
+  String helloMsg;
+  public String greet (String name) {
+    return(helloMsg + " " + name);
+  }
+
+  public void setHelloMsg (String msg) {
+    helloMsg = msg;
+  }
+}
