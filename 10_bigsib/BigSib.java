1873ed36f8d4996ad9fc0a8c51130cf1783d893a
Diana Akhmedova <dakhmedova30@stuy.edu>
2021-10-05 20:21:31 -0400

    init setup

--- 10_bigsib/BigSib.java
+++ 10_bigsib/BigSib.java
@@ -1,7 +1,6 @@
 public class BigSib {
   public static String greet (String name) {
-    System.out.println("Why, hello there, " + name + ". How do you do?");
-    return " ";
+    return("Why, hello there, " + name + ". How do you do?");
   }
 
 }
