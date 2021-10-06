9a13ed1b25b25b948462572d4604c0e5627719d0
Diana Akhmedova <90854530+Dianartemis@users.noreply.github.com>
2021-10-05 22:46:48 -0400

    Update BigSib.java

--- 12_bigsib/BigSib.java
+++ 12_bigsib/BigSib.java
@@ -1,3 +1,18 @@
+/*
+Diana Akhmedova + Rin Fk
+APCS
+HW 12 - On Elder Individuality and the Elimination of Radio Fuzz
+2021-10-05
+
+DISCO:
+- Remove static if the variable is an instance variable.
+- Remember to use "void" if you do not want an ouput.
+- Declare variables outside of methods.
+- You can declare variables without any specific values.
+QCC:
+- What is an instance variable?
+*/
+
 public class BigSib {
   String helloMsg;
   public String greet (String name) {
