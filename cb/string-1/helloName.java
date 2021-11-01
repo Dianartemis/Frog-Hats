1c56fed17dcb2524bacfec198bce4588a698dfdc
Diana Akhmedova <dakhmedova30@stuy.edu>
2021-11-01 01:28:29 -0400

    hw 28

--- cb/string-1/helloName.java
+++ cb/string-1/helloName.java
@@ -1,3 +1,18 @@
+/**
+Diana Akhmedova + Ajax
+APCS
+HW28: PPMP
+2021-10-31
+
+DISCO:
+- str.toLowerCase() returns the lowercase version of a String.
+
+QCC:
+- Why do the methods in CodingBat not include "static", even though we are not dealing with instance variables?
+
+Time Spent: 1.5 hours
+**/
+
 public class helloName {
 
   public static void main( String[]args ) {
