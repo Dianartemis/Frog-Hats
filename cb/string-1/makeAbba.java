905574042279a9d38cb3b4bf4429cb6c7cd60034
Diana Akhmedova <dakhmedova30@stuy.edu>
2021-10-31 21:27:02 -0400

    hw 28

--- cb/string-1/makeAbba.java
+++ cb/string-1/makeAbba.java
@@ -1,7 +1,11 @@
+import java.io.*;
+
 public class makeAbba {
 
   public static void main( String[]args ) {
-    System.out.println( "Hi", "Bye" );
+    System.out.println( makeAbba("Hi", "Bye") );
+    System.out.println( makeAbba("Yo", "Alice") );
+    System.out.println( makeAbba("What", "Up") );
   }
 
   public static String makeAbba( String a, String b ) {
