7a30e86cc806b22ab278ad44e802762db516aea1
Diana Akhmedova <dakhmedova30@stuy.edu>
2021-11-12 14:06:25 -0500

    hw 34

---
+++ 34_array/Loopier.java
@@ -0,0 +1,37 @@
+public class Loopier {
+
+  public static void main ( String[] args ) {
+
+  }
+
+  public static void intArray() {
+    int[] a = new int[5];
+  }
+
+  public static String arrayToString ( int[] ary ) {
+    String ans = "[";
+    for( int i = 0; i < ary.length; i++) {
+      ans += ary[i];
+      if( i != ary.length - 1 ) {
+        ans += ", ";
+      }
+    }
+    return ans + "]";
+  }
+
+  public static int linSearch( int[] a, int target ) {
+    int random = (int)(Math.random() * );
+  }
+
+  public static int linSearchR( int[] a, int target ) {
+
+  }
+
+  public static freq( int[] a, int target ) {
+
+  }
+
+  public static freqRec( int[] a, int target ) {
+    
+  }
+}
