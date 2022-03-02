2f61c8e3db50626d77f3727e86d22b1b63a916fc
Diana Akhmedova <dakhmedova30@stuy.edu>
2022-03-01 20:56:30 -0500

    hw 68

--- 68_knight/KnightTour.java
+++ 68_knight/KnightTour.java
@@ -52,20 +52,20 @@ public class KnightTour
     TourFinder tf = new TourFinder( n );
 
     //clear screen using ANSI control code
-    System.out.println( "[2J" );
+    System.out.println( "#[2J" );
 
     //display board
     System.out.println( tf );
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     //for fixed starting location, use line below:
-    tf.findTour( 2, 2, 1 );
+    tf.findTour( 2, 2, 1);
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     //for random starting location, use lines below:
-    //int startX = (int)(Math.random() * n);
-    //int startY = (int)(Math.random() * n);
+    //int startX = (int) (Math.random() * n);
+    //int startY = (int) (Math.random() * n);
     //tf.findTour( startX, startY, 1 );   // 1 or 0 ?
     //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
@@ -84,7 +84,7 @@ class TourFinder
   //instance vars
   private int[][] _board;
   private int _sideLength; //board has dimensions n x n
-  private boolean _solved = ???
+  private boolean _solved = false;
 
   //constructor -- build board of size n x n
   public TourFinder( int n )
@@ -92,12 +92,20 @@ class TourFinder
     _sideLength = n;
 
     //init 2D array to represent square board with moat
-    _board = int[n][n];
+    _board = new int[_sideLength + 4][_sideLength + 4];
 
     //SETUP BOARD --  0 for unvisited cell
     //               -1 for cell in moat
     //---------------------------------------------------------
-    ???
+    for (int x = 0; x < _board.length; x ++) {
+      for (int y = 0; y < _board.length; y ++) {
+        if (x == 0 || y == 0 || x == 1 || y == 1 || x == _board.length - 1 || y == _board.length - 1 || x == _board.length - 2 || y == _board.length - 2) {
+          _board[y][x] = -1;
+        } else {
+          _board[y][x] = 0;
+        }
+      }
+    }
     //---------------------------------------------------------
 
   }//end constructor
@@ -109,7 +117,7 @@ class TourFinder
   public String toString()
   {
     //send ANSI code "ESC[0;0H" to place cursor in upper left
-    String retStr = "[0;0H";
+    String retStr = "#[0;0H";
     //emacs shortcut: C-q, then press ESC
     //emacs shortcut: M-x quoted-insert, then press ESC
 
@@ -150,16 +158,17 @@ class TourFinder
     //delay(50); //slow it down enough to be followable
 
     //if a tour has been completed, stop animation
-    if ( ??? ) System.exit(0);
+    if ( moves == _sideLength * _sideLength + 1 ) System.exit(0);
 
     //primary base case: tour completed
-    if ( ??? ) {
-      ???
+    if ( moves == _sideLength * _sideLength + 1 ) {
+      System.out.println(toString());
+      _solved = true;
       System.out.println( this ); //refresh screen
       return;
     }
     //other base case: stepped off board or onto visited cell
-    if ( board[x][y] != 0 ) {
+    if ( _board[x][y] != 0 ) {
       return;
     }
     //otherwise, mark current location
@@ -182,18 +191,20 @@ class TourFinder
        *     g . . . b
        *     . h . a .
       ******************************************/
-      findTour(x + 2, y + 1, moves + 1);
-      findTour(x + 2, y - 1, moves + 1);
-      findTour(x - 2, y + 1, moves + 1);
-      findTour(x - 2, y - 1, moves + 1);
-      findTour(x + 1, y + 2, moves + 1);
-      findTour(x + 1, y - 2, moves + 1);
-      findTour(x - 1, y + 2, moves + 1);
-      findTour(x - 1, y - 2, moves + 1);
+      findTour(x + 1, y + 2, moves + 1); // a
+      findTour(x + 2, y + 1, moves + 1); // b
+      findTour(x + 2, y - 1, moves + 1); // c
+      findTour(x + 1, y - 2, moves + 1); // d
+      findTour(x - 1, y - 2, moves + 1); // e
+      findTour(x - 2, y - 1, moves + 1); // f
+      findTour(x - 2, y + 1, moves + 1); // g
+      findTour(x - 1, y + 2, moves + 1); // h
+
+
 
       //If made it this far, path did not lead to tour, so back up...
       // (Overwrite number at this cell with a 0.)
-        board[x][y] = 0;
+      _board[x][y] = 0;
 
       System.out.println( this ); //refresh screen
     }
