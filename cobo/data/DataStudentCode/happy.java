1008625d709fe37f6c3c7e2b32ce5ec3dc15044b
Diana Akhmedova <90854530+Dianartemis@users.noreply.github.com>
2022-04-06 16:03:13 -0400

    l08

--- cobo/data/DataStudentCode/happy.java
+++ cobo/data/DataStudentCode/happy.java
@@ -2,7 +2,7 @@ public class happy{
   private String country;
   private int rank;
 
-  happy(String country, int rank) {
+  public happy(String country, int rank) {
      this.country = country;
      this.rank = rank;
   }
