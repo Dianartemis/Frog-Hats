d84852baa947683f7185ba33defa5ebdc52ef788
Diana Akhmedova <dakhmedova30@stuy.edu>
2022-05-13 13:55:09 -0400

    hw 99

---
+++ 99_alpq/ArrayPriorityQueue.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class ArrayPriorityQueue implements PriorityQueue {
+
+  ArrayList<Integer> al;
+
+  public ArrayPriorityQueue()
+
+  public void add(Integer e) {
+    al = new ArrayList<Integer>();
+  }
+
+  public boolean isEmpty() {
+
+  }
+
+  public Integer peekMin() {
+
+  }
+
+  public Integer removeMin() {
+
+  }
+
+}
---
+++ 99_alpq/PriorityQueue.java
@@ -0,0 +1,8 @@
+public interface PriorityQueue {
+
+  public void add(Integer e);
+  public boolean isEmpty();
+  public Integer peekMin();
+  public Integer removeMin();
+
+}
